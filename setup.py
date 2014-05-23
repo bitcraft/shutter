04d73541f00df5f31fc57cbbdd61da661709f69d
Leif Theden <leif.theden@gmail.com>
2014-05-23 12:41:22 -0500

    segfault

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 
 setup(name='shutter',
-      version='0.0.5',
+      version='0.0.6',
       description='ctypes interface for libgphoto2',
       author='bitcraft',
       author_email='leif.theden@gmail.com',
